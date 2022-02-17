ccda63f7def07618aae1c3e053519be1bebe319b
Luis mi <alexanderlara1996@gmail.com>
2022-02-17 10:13:34 -0500

    Guardando cambios

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+class App extends Component {
+
+  render() {
+    return (
+      <>
+        <Header />
+        <h1>Welcome</h1>
+      </>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <header>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Otro</a></li>
+                </ul>
+            </header>
+        );
+    }
+
+}
+
+export default Header;
