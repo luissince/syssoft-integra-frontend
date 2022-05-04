7f73e352a6d2f0fa969596bea1a8c30e51de37a9
Luis mi <alexanderlara1996@gmail.com>
2022-05-04 08:27:31 -0500

    'act'

--- app/src/components/principal/Principal.js
+++ app/src/components/principal/Principal.js
@@ -18,6 +18,7 @@ class Principal extends React.Component {
             loadModal: true,
             msgModal: "Cargando proyectos...",
         }
+        this.refTxtSearch = React.createRef();
 
         this.abortControllerTable = new AbortController();
     }
@@ -50,6 +51,10 @@ class Principal extends React.Component {
 
     componentWillUnmount() {
         this.abortControllerTable.abort();
+    }
+
+    onEventSearch = async (value) => {
+        console.log(value);
     }
 
     onEventSignIn = async (event) => {
@@ -91,90 +96,98 @@ class Principal extends React.Component {
                         <div className="clearfix absolute-all bg-white">
                             {spinnerLoading(this.state.msgModal)}
                         </div>
-                        : null
-                    }
+                        :
+                        <>
 
-                    <div className='row'>
-                        <div className='col-md-3 col-12' >
-                            <div className='d-flex h-100 justify-content-start align-items-center'>
-                                <div className="form-group">
-                                    <img className="img-fluid" src={logoInmobiliaria} alt="logo" width="140" />
+                            <div className='row'>
+                                <div className='col-md-3 col-12' >
+                                    <div className='d-flex h-100 justify-content-start align-items-center'>
+                                        <div className="form-group">
+                                            <img className="img-fluid" src={logoInmobiliaria} alt="logo" width="140" />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='col-md-6 col-12' >
+                                    <div className='d-flex h-100 flex-column justify-content-center align-items-center'>
+                                        <div className="form-group text-center">
+                                            <h3 className="text-dark">Empresa Inmobiliaria sac</h3>
+                                            <h5 className="text-secondary">Ruc: 12345678952</h5>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='col-md-3 col-12' >
+                                    <div className='d-flex h-100 justify-content-end align-items-center'>
+                                        <div className="form-group">
+                                            <button onClick={this.onEventSignIn} className="btn btn-outline-success" type="button">
+                                                <i className="bi bi-box-arrow-right"></i>
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className='col-md-6 col-12' >
-                            <div className='d-flex h-100 flex-column justify-content-center align-items-center'>
-                                <div className="form-group text-center">
-                                    <h3 className="text-dark">Empresa Inmobiliaria sac</h3>
-                                    <h5 className="text-secondary">Ruc: 12345678952</h5>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='col-md-3 col-12' >
-                            <div className='d-flex h-100 justify-content-end align-items-center'>
-                                <div className="form-group">
-                                    <button onClick={this.onEventSignIn} className="btn btn-outline-success" type="button">
-                                        <i className="bi bi-box-arrow-right"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-md-9 col-sm-8 col-12">
-                            <div className="form-group">
-                                <input className="form-control mr-sm-2 bg-transparent" placeholder="filtar por proyecto o nombre del proyecto" aria-label="Search" />
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-4 col-12">
-                            <div className="form-group">
-                                <button className="w-100 btn btn-outline-success my-2 my-sm-0" type="button" >Filtrar proyecto</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        {
-                            this.state.data.map((item, index) => (
-                                <div key={index} className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <div className="row">
+                                <div className="col-md-9 col-sm-8 col-12">
                                     <div className="form-group">
-                                        <div className="card">
-                                            <img src={item.imagen === "" ? noImage : `data:image/${item.extensionimagen};base64,${item.imagen}`} alt="" className="card-img-top" />
-                                            <div className="card-body m-2">
-                                                <h6 className='text-info font-weight-bold'>{item.nombre}</h6>
-                                                <h6 className='text-secondary'>{item.ubicacion}</h6>
-                                                <button onClick={() => this.onEventIngresar(item)} type="button" className="btn btn-block btn-outline-primary text-info" >
-                                                    <i className="bi bi-arrow-right-circle-fill"></i> Ingresar
-                                                </button>
-                                            </div>
-                                            <hr className="m-0" />
-                                            <div className="card-body m-2">
-                                                <ul className="list-group text-left pt-0">
-                                                    <li className="list-group-item border-0 px-0 pt-0"><i className="bi bi-geo-fill"></i> Moneda {item.moneda}({item.simbolo})</li>
-                                                    <li className="list-group-item border-0 px-0"><i className="bi bi-geo-fill"></i> Total de lotes {item.lotes.length}</li>
-                                                    <li className="list-group-item border-0 px-0"><i className="bi bi-geo-fill"></i> Lotes disponibles {item.lotes.filter(lote => lote.estado === 1).length}</li>
-                                                    <li className="list-group-item border-0 px-0"><i className="bi bi-grid-1x2-fill"></i>
-                                                        {
-                                                            item.estado === 1 ? <span className='text-success'> Estado en Venta</span> : <span className='text-danger'> Estado en Litigio</span>
-                                                        }</li>
-                                                </ul>
-                                                {/* <div className="row pt-3">
+                                        <input
+                                            className="form-control mr-sm-2 bg-transparent"
+                                            placeholder="filtar por proyecto o nombre del proyecto"
+                                            aria-label="Search"
+                                            ref={this.refTxtSearch}
+                                            onKeyUp={(event) => this.onEventSearch(event.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="col-md-3 col-sm-4 col-12">
+                                    <div className="form-group">
+                                        <button className="w-100 btn btn-outline-success my-2 my-sm-0" type="button" >Filtrar proyecto</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                {
+                                    this.state.data.map((item, index) => (
+                                        <div key={index} className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                                            <div className="form-group">
+                                                <div className="card">
+                                                    <img src={item.imagen === "" ? noImage : `data:image/${item.extensionimagen};base64,${item.imagen}`} alt="" className="card-img-top" />
+                                                    <div className="card-body m-2">
+                                                        <h6 className='text-info font-weight-bold'>{item.nombre}</h6>
+                                                        <h6 className='text-secondary'>{item.ubicacion}</h6>
+                                                        <button onClick={() => this.onEventIngresar(item)} type="button" className="btn btn-block btn-outline-primary text-info" >
+                                                            <i className="bi bi-arrow-right-circle-fill"></i> Ingresar
+                                                        </button>
+                                                    </div>
+                                                    <hr className="m-0" />
+                                                    <div className="card-body m-2">
+                                                        <ul className="list-group text-left pt-0">
+                                                            <li className="list-group-item border-0 px-0 pt-0"><i className="bi bi-geo-fill"></i> Moneda {item.moneda}({item.simbolo})</li>
+                                                            <li className="list-group-item border-0 px-0"><i className="bi bi-geo-fill"></i> Total de lotes {item.lotes.length}</li>
+                                                            <li className="list-group-item border-0 px-0"><i className="bi bi-geo-fill"></i> Lotes disponibles {item.lotes.filter(lote => lote.estado === 1).length}</li>
+                                                            <li className="list-group-item border-0 px-0"><i className="bi bi-grid-1x2-fill"></i>
+                                                                {
+                                                                    item.estado === 1 ? <span className='text-success'> Estado en Venta</span> : <span className='text-danger'> Estado en Litigio</span>
+                                                                }</li>
+                                                        </ul>
+                                                        {/* <div className="row pt-3">
                                                     <div className="col-12">
                                                         <div className="progress">
                                                             <div className="progress-bar bg-info w-10" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">50%</div>
                                                         </div>
                                                     </div>
                                                 </div> */}
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
+                                    ))
+                                }
+                            </div>
+                        </>
+                    }
+
                 </div>
             </>
         );
