c3c86d71ac3c528fa8a7e7dd8e2568691cb3cbc9
Ruber Flores <ruberfc@gmail.com>
2022-07-01 09:21:29 -0500

    'act'

--- app/src/components/ajustes/proyecto/ProcesoProyecto.js
+++ app/src/components/ajustes/proyecto/ProcesoProyecto.js
@@ -186,6 +186,7 @@ class ProcesoProyecto extends React.Component {
             });
             this.selectItem = true;
         } catch (error) {
+            console.log(error)
             if (error.message !== "canceled") {
                 await this.setStateAsync({
                     msgLoading: "Se produjo un error un interno, intente nuevamente."
