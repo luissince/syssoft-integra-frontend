19bbf13ab7b75512d83a0d88049c288e2580e10f
Ruber Flores <ruberfc@gmail.com>
2022-07-01 09:11:23 -0500

    'act'

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ app.use(cors());
  */
 app.set('port', process.env.PORT || 5000);
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: false }));
+app.use(express.json({ limit: '10240mb' }));
+app.use(express.urlencoded({ limit: '1024mb', extended: false }));
 
 /**
  * 
