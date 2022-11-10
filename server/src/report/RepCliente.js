bc99c386cb3e75b3f46486d68b296bc1bfa85a06
Ruber Flores <ruberfc@gmail.com>
2022-11-09 19:19:30 -0500

    'act'

--- server/src/report/RepCliente.js
+++ server/src/report/RepCliente.js
@@ -517,7 +517,7 @@ class RepCliente {
             );
 
             doc.fontSize(h3).text(
-                req.query.porProyecto == "" ? "TODOS LOS PROYECTOS" : `PROYECTO: ${req.query.nombreProyecto}`,
+                req.query.porProyecto == "0" ? "TODOS LOS PROYECTOS" : `PROYECTO: ${req.query.nombreProyecto}`,
                 orgX,
                 doc.y + 10,
                 {
