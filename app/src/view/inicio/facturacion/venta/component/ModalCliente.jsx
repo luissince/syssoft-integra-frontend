e602ff48cde2129462f6769f323f2bc60e73bd5f
Luis Lara <xanderls25@icloud.com>
2023-10-26 06:13:54 -0500

    Guardando cambios del modulo de ventas

---
+++ app/src/view/inicio/facturacion/venta/component/ModalCliente.jsx
@@ -0,0 +1,49 @@
+const ModalCliente = (props) => {
+
+    const { idImpuesto, refImpuesto, impuestos, handleSelectImpuesto } = props;
+
+    const { idMoneda, refMoneda, monedas, handleSelectMoneda } = props;
+
+    const { handleSaveCliente, handleCloseOverlayCliente, handleCloseCliente } = props;
+
+    return (
+        <div id="side-model-invoice" className='side-modal'>
+            <div className='side-modal_wrapper'>
+                <div className="card border-0 rounded-0">
+                    <div className="card-header">Configuración de Venta</div>
+                    <button
+                        type="button"
+                        className="close"
+                        aria-label="Close"
+                        onClick={handleCloseCliente}>
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
+                    <div className="card-body">
+
+
+                    </div>
+
+                    <div className="card-footer bg-white">
+                        <div className="d-flex align-items-center justify-content-between">
+                            <span className="d-block">Campos obligatorios <i className="fa fa-asterisk text-danger small"></i></span>
+                            <div>
+                                <button className='btn btn-outline-success mr-2' onClick={handleSaveCliente}>Aceptar</button>
+                                <button className='btn btn-outline-secondary ' onClick={handleCloseCliente}>Cancelar</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div className='side-modal_bottom'>
+                </div>
+            </div>
+            <div className="side-modal_overlay" onClick={handleCloseOverlayCliente}>
+            </div>
+        </div>
+    );
+}
+
+export default ModalCliente;
