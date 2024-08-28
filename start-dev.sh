4295c295c8501b3f1bec1b6eb9cd215578ba2876
Luis Lara <xanderls25@icloud.com>
2024-08-28 15:52:03 -0500

    El archivo start-dev.sh tiene nueva funcionalidad para elegir su rama

--- start-dev.sh
+++ start-dev.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# Verifica si se pasó el flag '--stay' para permanecer en la rama actual
+stay_in_current_branch=false
+for arg in "$@"; do
+  if [ "$arg" == "--stay" ]; then
+    stay_in_current_branch=true
+  fi
+done
+
 # Verifica la rama actual
 current_branch=$(git branch --show-current)
 
-# Si no estás en la rama 'development', cámbiate
-if [ "$current_branch" != "development" ]; then
+# Si no estás en la rama 'development' y no se pasó el flag '--stay', cámbiate
+if [ "$current_branch" != "development" ] && [ "$stay_in_current_branch" == false ]; then
     echo "Advertencia: No estás en la rama 'development'. Estás en '$current_branch'."
     echo "Cambiando a la rama 'development'..."
     git checkout development
 fi
 
-vite
+npx nodemon index.js
